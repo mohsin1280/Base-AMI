609521d90bda9996ac5161d1063e23740005d847
Mohsin Mohammed <108212324+mohsin1280@users.noreply.github.com>
2023-07-10 10:28:12 +0530

    Update provisioner.sh
    for Amazon Linux

--- provisioner.sh
+++ provisioner.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 # update package
-sudo apt update
-sudo apt upgrade -y
+sudo yum -y update
 
 # install git
-sudo apt install -y git
+sudo yum install -y git
 
 # install SSM
-curl "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb" -o "amazon-ssm-agent.deb"
-sudo dpkg -i amazon-ssm-agent.deb
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+#sudo systemctl start amazon-ssm-agent
 
 # install CloudWatch Agent
-curl "https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb" -o "amazon-cloudwatch-agent.deb"
-sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo yum install -y https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 
 # install AWS Inspector
 curl -O https://inspector-agent.amazonaws.com/linux/latest/install
 sudo bash install
 
 # install Docker
-sudo apt install -y docker.io
+sudo yum -y install docker
 sudo systemctl start docker
